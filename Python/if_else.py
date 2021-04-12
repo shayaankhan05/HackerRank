1b3128330b43e2c514c036a2bf20319fe8f4602d
Shayaan <you@example.com>
2021-04-12 14:10:35 +0530

    all test cases passed

--- Python/if_else.py
+++ Python/if_else.py
@@ -1,3 +1,5 @@
+
+
 import math
 import os
 import random
@@ -6,13 +8,13 @@ import sys
 
 
 if __name__ == '__main__':
-    n = int(input().strip())
+    n = int(raw_input().strip())
 
     if (n % 2 != 0):
         print("Weird")
-    elif (n % 2 == 0) and (n in range(2, 5)):
+    elif (n % 2 == 0) and (n in range(2, 6)):
         print("Not Weird")
-    elif (n % 2 == 0) and (n in range(6, 20)):
+    elif (n % 2 == 0) and (n in range(6, 21)):
         print("Weird")
     elif (n % 2 == 0) and (n > 20):
         print("Not Weird")
